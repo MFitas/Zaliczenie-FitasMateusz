97f8248864fcb95658a66d09ae637a82d706c157
Mateusz Fitas <w65998@student.wsiz.edu.pl>
2022-07-10 10:01:17 +0200

    Pusty projekt

---
+++ ConsoleApplication1/ConsoleApplication1/Zaliczenie Fitas Mateusz.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+}
+
+uture, to open this project again, go to File > Open > Project and select the .sln file
